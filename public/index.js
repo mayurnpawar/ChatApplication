a747252518e70ab65a4ae1a2066d938a319b77f3
Pawar <cwr.Mayur.Pawar1@int.asurion.com>
2017-01-03 10:12:13 -0600

    sadasdasd

--- public/index.js
+++ public/index.js
@@ -2,12 +2,19 @@ var socket = io();
 
 socket.on('connect',function() {
   console.log('Connected to server');
-});
+  });
 
+socket.emit('createMessage',{
+    To: 'Mayur',
+    Text : 'Hey How R U'
+  });
 socket.on('disconnect',function() {
   console.log('Disconneced from server');
 });
 
 socket.on('newEmail', function(email){
  console.log('New eMail',email);
+});
+socket.on('newMessage', function(Text){
+ console.log('New Message Created',Text);
 });
--- server/server.js
+++ server/server.js
@@ -13,11 +13,17 @@ app.use(express.static(publicpath));
 
 io.on('connection',(socket)=>{
  console.log('New User Connected');
-
- socket.emit('newEmail',{
-   From : 'mayurnpawar@gmail.com',
-   Subject : 'How r you',
-   Received : 23432
+ // socket.emit('newEmail',{
+ //   From : 'mayurnpawar@gmail.com',
+ //   Subject : 'How r you',
+ //   Received : 23432
+ // });
+ socket.emit('newMessage' , {
+   Text : 'Hey I am fine',
+   CreatedAt : 'Now'
+ });
+ socket.on('createMessage',(TextData)=>{
+   console.log(`Text Recevied from Client : ${TextData.Text}`);
  });
  socket.on('disconnect',()=>{
  console.log('User was disconnected');
